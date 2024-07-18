77e40c56ac45ed315d3d6266f7ed99fbcd299109
sulekhasharma <Sulekha.sharma@nagarro.com>
2024-07-18 20:07:36 +0530

    refactor: Added Edge Browser support

--- src/main/java/config/WebDriverSetup.java
+++ src/main/java/config/WebDriverSetup.java
@@ -3,20 +3,39 @@ package config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class WebDriverSetup {
     protected static WebDriver driver;
 
-    public static void setupDriver(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\sulekhasharma\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("--start-maximized");
-        boolean headless=ConfigLoader.getBooleanProperty("headless",false);
-        if(headless){
-            options.addArguments("--headless");
+    public static void setupDriver() {
+        String browser = ConfigLoader.getProperty("browser");
+        boolean headless = ConfigLoader.getBooleanProperty("headless", false);
+
+        switch (browser.toLowerCase()) {
+            case "edge":
+                System.setProperty("webdriver.edge.driver", ConfigLoader.getProperty("edgeDriverPath"));
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--inprivate");
+                edgeOptions.addArguments("--start-maximized");
+                if (headless) {
+                    edgeOptions.addArguments("--headless");
+                }
+                driver = new EdgeDriver(edgeOptions);
+                break;
+            case "chrome":
+            default:
+                System.setProperty("webdriver.chrome.driver", ConfigLoader.getProperty("chromeDriverPath"));
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--incognito");
+                chromeOptions.addArguments("--start-maximized");
+                if (headless) {
+                    chromeOptions.addArguments("--headless");
+                }
+                driver = new ChromeDriver(chromeOptions);
+                break;
         }
-        driver =new ChromeDriver(options);
     }
 
     public static void tearDownDriver() {
@@ -25,7 +44,7 @@ public class WebDriverSetup {
         }
     }
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         return driver;
     }
 }
